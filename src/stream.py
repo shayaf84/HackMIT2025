520499b5bf78c2cf724a11636e1ec9d3e0970257
shayaf84 <shayafmd@gmail.com>
2025-09-13 17:15:50 -0400

    identify center of laser pointer in droidcam

--- src/stream.py
+++ src/stream.py
@@ -15,17 +15,38 @@ while True:
         print("Error: Could not read frame from DroidCam.")
         break
 
-    relative = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+    rotated = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
 
-    height, width, _ = relative.shape
-    relative = relative / 255.0
+    height, width, _ = rotated.shape
+    relative = rotated / 255.0
     relative -= relative.mean(axis = 2, keepdims = True)
     relative = relative[..., 2]
-    relative = np.where(relative > 0.4, relative, 0.0)
-    relative = np.stack((relative, relative, relative), axis = -1)
-    
+    relative = np.where(relative > 0.3, relative, 0.0)
 
-    cv2.imshow("DroidCam Feed", relative)
+    weights = relative.flatten()
+    weights_sum = np.sum(weights)
+
+
+    if weights_sum > 1.0:
+        idx = np.arange(height * width)
+        xs = idx % width
+        ys = idx // width
+        center_x = np.sum(xs * weights) / weights_sum
+        center_y = np.sum(ys * weights) / weights_sum
+
+        rotated = cv2.ellipse(
+            rotated,
+            (int(center_x), int(center_y)),
+            (10, 10),
+            angle = 0,
+            startAngle = 0,
+            endAngle = 360,
+            color = (255, 0, 0),
+            thickness = 2
+        )
+    cv2.imshow("DroidCam Feed", rotated)
+
+
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
